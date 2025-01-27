0ddbf9fc796c8fb1e87e3f8f0eb2d9f360e56495
Serhii Pustovit <sergioo040199@gmail.com>
2025-01-27 18:32:45 +0200

    New data and advices added

--- Scuchno/data.hpp
+++ Scuchno/data.hpp
@@ -1,4 +1,5 @@
 #include <map>
+#include <vector>
 #include <string>
 
 #include "types.hpp"
@@ -7,8 +8,13 @@ namespace scu {
 
 	struct Data
 	{
+		static const std::string age_trait_id;
+
 		static const std::map<std::string, Trait> traits;
 		static const std::map<std::string, Stage> stages;
+
+		static const std::vector<Ending> endings;
+		static const std::vector<DeathDescription> death_descriptions;
 	};
 
 }
--- Scuchno/types.hpp
+++ Scuchno/types.hpp
@@ -4,13 +4,13 @@
 namespace scu {
 
 	struct Trait {
-		std::string name;
-		std::string description;
+		std::wstring name;
+		std::wstring description;
 		std::int32_t value;
 	};
 
 	struct Choice {
-		std::string description;
+		std::wstring description;
 
 		std::map<std::string, std::int32_t> effects;
 		std::map<std::string, std::int32_t> chains;
@@ -18,7 +18,7 @@ namespace scu {
 
 	struct Event
 	{
-		std::string description;
+		std::wstring description;
 
 		std::map<std::string, std::int32_t> conditions_traits_min;
 		std::map<std::string, std::int32_t> conditions_traits_max;
@@ -32,13 +32,33 @@ namespace scu {
 
 	struct Stage
 	{
-		std::string name;
-		std::string description;
+		std::wstring name;
+		std::wstring description;
 
 		// In month
 		std::uint32_t min_age;
 		std::uint32_t max_age;
 
 		std::map<std::string, Event> events;
+	};
+
+	struct Ending
+	{
+		std::wstring description;
+
+		bool is_condition_max = false;
+		std::string condition_trait_id;
+		std::int32_t condition_trait_value;
+	};
+
+	struct DeathDescription {
+		/*DeathDescription(std::string, std::string, std::int32_t, std::int32_t) {
+
+		}*/
+
+		std::wstring description;
+		std::string trait_id;
+		std::int32_t min_value;
+		std::int32_t max_value;
 	};
 }
